b865bb89964fe995d11f63c98e1a5f5034162a65
xumones <toastpork13@gmail.com>
2021-06-06 23:49:02 +0700

    exercise 4 first

--- assignments/Exercise4_Rawipote_P.py
+++ assignments/Exercise4_Rawipote_P.py
@@ -1,1 +1,10 @@
+Foundation_English = 62.6
+General_Business = 82.1
+IntroductionToComputerSystems = 76
+ComputerProgramming = 59.7
 
+print("---Your Score---")
+print("Foundation English :", Foundation_English)
+print("General Business :", General_Business)
+print("Introduction to Computer System :", IntroductionToComputerSystems)
+print("Computer Programming :", ComputerProgramming)
