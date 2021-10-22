f7be1365f8e92010c41d207ee85e0d6fbd9f1eba
xumones <toastpork13@gmail.com>
2021-10-22 15:44:48 +0700

    Lecture 50 Rawipote_P

---
+++ assignments/Lecture50 Rawipote_P.py
@@ -0,0 +1,13 @@
+def plusNumber(x,y):
+    print(x+y)
+def minusNumber(x,y):
+    print(x-y)
+def multiplyNumber(x,y):
+    print(x*y)
+def divideNumber(x,y):
+    print(int(x/y))
+
+plusNumber(20,10)
+minusNumber(90,20)
+multiplyNumber(52,2)
+divideNumber(10,2)
